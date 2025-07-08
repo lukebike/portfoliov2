eadc3dcf9672ac7723b49df3bbc3014f0ec698b4
lukebike <lukesalem2002@gmail.com>
2025-07-08 17:50:06 +0200

    Fixed some typos

--- src/info.ts
+++ src/info.ts
@@ -1,7 +1,7 @@
 const aboutItems = [
-  "I'm a full-stack student at Teknikhögskolan Lund, based in Malmö, Sweden",
-  "I'm very passionate about software development and find a lot of enjoyment in backend work but open to frontend as well.",
-  "I am currently learning React to polish my frontend skills by building projects with React, this being one of them. Other than that I find enormous interest in backend and always look forward to delve deeper into Java and Spring Boot to sharpen my skills.",
+  "I'm an aspiring full-stack developer at Teknikhögskolan Lund, based in Malmö, Sweden",
+  "I'm very passionate about software development and find a lot of enjoyment in work entailing both the front and the back-end",
+  "I am currently learning React to polish my frontend skills by building projects with React, this being one of them. Other than that I also find enormous interest in backend and always look forward to delve deeper into Java and Spring Boot to sharpen my skills.",
   "Outside of my studies, I enjoy going to the gym, learning new languages and play a bit of video games.",
 ];
 
